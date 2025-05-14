1e8b5d01cb0e34184a450a96e19bf0cebf35d0ab
srimani <srimani0723@gmail.com>
2025-05-14 21:42:43 +0530

    update ...

--- dist/services/bookService.js
+++ dist/services/bookService.js
@@ -2,69 +2,68 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.bulkCreateBooks = exports.deleteBook = exports.updateBook = exports.createBook = exports.bookById = exports.allBooks = void 0;
 const uuid_1 = require("uuid");
-let books = [];
-// [
-//   {
-//     id: "1",
-//     title: "Wings of Fire",
-//     author: "A.P.J. Abdul Kalam",
-//     publishedYear: 1999,
-//   },
-//   {
-//     id: "2",
-//     title: "The White Tiger",
-//     author: "Aravind Adiga",
-//     publishedYear: 2008,
-//   },
-//   {
-//     id: "3",
-//     title: "Train to Pakistan",
-//     author: "Khushwant Singh",
-//     publishedYear: 1956,
-//   },
-//   {
-//     id: "4",
-//     title: "Godan",
-//     author: "Munshi Premchand",
-//     publishedYear: 1936,
-//   },
-//   {
-//     id: "5",
-//     title: "India After Gandhi",
-//     author: "Ramachandra Guha",
-//     publishedYear: 2007,
-//   },
-//   {
-//     id: "6",
-//     title: "The Discovery of India",
-//     author: "Jawaharlal Nehru",
-//     publishedYear: 1946,
-//   },
-//   {
-//     id: "7",
-//     title: "An Era of Darkness",
-//     author: "Shashi Tharoor",
-//     publishedYear: 2016,
-//   },
-//   {
-//     id: "8",
-//     title: "The Inheritance of Loss",
-//     author: "Kiran Desai",
-//     publishedYear: 2006,
-//   },
-//   {
-//     id: "9",
-//     title: "A Suitable Boy",
-//     author: "Vikram Seth",
-//     publishedYear: 1993,
-//   },
-//   {
-//     id: "10",
-//     title: "The Immortals of Meluha",
-//     author: "Amish Tripathi",
-//     publishedYear: 2010,
-//   },
-// ];
+let books = [
+    {
+        id: "1",
+        title: "Wings of Fire",
+        author: "A.P.J. Abdul Kalam",
+        publishedYear: 1999,
+    },
+    {
+        id: "2",
+        title: "The White Tiger",
+        author: "Aravind Adiga",
+        publishedYear: 2008,
+    },
+    {
+        id: "3",
+        title: "Train to Pakistan",
+        author: "Khushwant Singh",
+        publishedYear: 1956,
+    },
+    {
+        id: "4",
+        title: "Godan",
+        author: "Munshi Premchand",
+        publishedYear: 1936,
+    },
+    {
+        id: "5",
+        title: "India After Gandhi",
+        author: "Ramachandra Guha",
+        publishedYear: 2007,
+    },
+    {
+        id: "6",
+        title: "The Discovery of India",
+        author: "Jawaharlal Nehru",
+        publishedYear: 1946,
+    },
+    {
+        id: "7",
+        title: "An Era of Darkness",
+        author: "Shashi Tharoor",
+        publishedYear: 2016,
+    },
+    {
+        id: "8",
+        title: "The Inheritance of Loss",
+        author: "Kiran Desai",
+        publishedYear: 2006,
+    },
+    {
+        id: "9",
+        title: "A Suitable Boy",
+        author: "Vikram Seth",
+        publishedYear: 1993,
+    },
+    {
+        id: "10",
+        title: "The Immortals of Meluha",
+        author: "Amish Tripathi",
+        publishedYear: 2010,
+    },
+];
 const allBooks = () => books;
 exports.allBooks = allBooks;
 const bookById = (id) => books.find((book) => book.id === id);
--- services/bookService.ts
+++ services/bookService.ts
@@ -1,69 +1,68 @@
 import { v4 as uuidv4 } from "uuid";
 import { Book } from "../models/bookModel";
 
-let books: Book[] = [];
-// [
-//   {
-//     id: "1",
-//     title: "Wings of Fire",
-//     author: "A.P.J. Abdul Kalam",
-//     publishedYear: 1999,
-//   },
-//   {
-//     id: "2",
-//     title: "The White Tiger",
-//     author: "Aravind Adiga",
-//     publishedYear: 2008,
-//   },
-//   {
-//     id: "3",
-//     title: "Train to Pakistan",
-//     author: "Khushwant Singh",
-//     publishedYear: 1956,
-//   },
-//   {
-//     id: "4",
-//     title: "Godan",
-//     author: "Munshi Premchand",
-//     publishedYear: 1936,
-//   },
-//   {
-//     id: "5",
-//     title: "India After Gandhi",
-//     author: "Ramachandra Guha",
-//     publishedYear: 2007,
-//   },
-//   {
-//     id: "6",
-//     title: "The Discovery of India",
-//     author: "Jawaharlal Nehru",
-//     publishedYear: 1946,
-//   },
-//   {
-//     id: "7",
-//     title: "An Era of Darkness",
-//     author: "Shashi Tharoor",
-//     publishedYear: 2016,
-//   },
-//   {
-//     id: "8",
-//     title: "The Inheritance of Loss",
-//     author: "Kiran Desai",
-//     publishedYear: 2006,
-//   },
-//   {
-//     id: "9",
-//     title: "A Suitable Boy",
-//     author: "Vikram Seth",
-//     publishedYear: 1993,
-//   },
-//   {
-//     id: "10",
-//     title: "The Immortals of Meluha",
-//     author: "Amish Tripathi",
-//     publishedYear: 2010,
-//   },
-// ];
+let books: Book[] = [
+  {
+    id: "1",
+    title: "Wings of Fire",
+    author: "A.P.J. Abdul Kalam",
+    publishedYear: 1999,
+  },
+  {
+    id: "2",
+    title: "The White Tiger",
+    author: "Aravind Adiga",
+    publishedYear: 2008,
+  },
+  {
+    id: "3",
+    title: "Train to Pakistan",
+    author: "Khushwant Singh",
+    publishedYear: 1956,
+  },
+  {
+    id: "4",
+    title: "Godan",
+    author: "Munshi Premchand",
+    publishedYear: 1936,
+  },
+  {
+    id: "5",
+    title: "India After Gandhi",
+    author: "Ramachandra Guha",
+    publishedYear: 2007,
+  },
+  {
+    id: "6",
+    title: "The Discovery of India",
+    author: "Jawaharlal Nehru",
+    publishedYear: 1946,
+  },
+  {
+    id: "7",
+    title: "An Era of Darkness",
+    author: "Shashi Tharoor",
+    publishedYear: 2016,
+  },
+  {
+    id: "8",
+    title: "The Inheritance of Loss",
+    author: "Kiran Desai",
+    publishedYear: 2006,
+  },
+  {
+    id: "9",
+    title: "A Suitable Boy",
+    author: "Vikram Seth",
+    publishedYear: 1993,
+  },
+  {
+    id: "10",
+    title: "The Immortals of Meluha",
+    author: "Amish Tripathi",
+    publishedYear: 2010,
+  },
+];
 
 export const allBooks = (): Book[] => books;
 
